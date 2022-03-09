d061c7b43bebfc4bf030513ebaf88c63e3012185
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-09 16:29:08 +0530

    AnalyseMood

--- MoodAnalysersTest/UnitTest1.cs
+++ MoodAnalysersTest/UnitTest1.cs
@@ -1,13 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
 
-namespace MoodAnalysersTest
+namespace MoodTest;
+
+[TestClass]
+public class TestMood
 {
-    [TestClass]
-    public class UnitTest1
+    /// <summary>
+    /// Tests for sad mood.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    [TestMethod]
+    [DataRow("I am in Sad Mood")]
+    public void TestSadMood(string message)
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+        string result = moodAnalyser.AnalyseMood();
+        Assert.AreEqual("Sad", result);
+    }
+
+    /// <summary>
+    /// Tests for happy mood.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    [TestMethod]
+    [DataRow("I am in any Mood")]
+    public void TestHappyMood(string message)
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+        string result = moodAnalyser.AnalyseMood();
+        Assert.AreEqual("Happy", result);
     }
 }
