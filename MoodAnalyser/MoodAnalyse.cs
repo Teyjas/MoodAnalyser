3c1bd0ded814a9fc119cdbdf34670008d0086e41
Teyjas <97289921+Teyjas@users.noreply.github.com>
2022-03-09 16:23:29 +0530

    AnalyseMood

---
+++ MoodAnalyser/MoodAnalyse.cs
@@ -0,0 +1,26 @@
+﻿namespace MoodAnalyzer;
+
+public class MoodAnalyser
+{
+    string message;
+
+    public MoodAnalyser()
+    {
+        message = "";
+    }
+
+    public MoodAnalyser(string message)
+    {
+        this.message = message;
+    }
+
+    /// <summary>
+    /// Analyses the mood.
+    /// </summary>
+    public string AnalyseMood()
+    {
+        if (message.Contains("Sad", StringComparison.OrdinalIgnoreCase) is true)
+            return "Sad";
+        return "Happy";
+    }
+}
--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using MoodAnalyzer;
+
+MoodAnalyser mood = new MoodAnalyser("I am really sad");
+
+string moodStatus = mood.AnalyseMood();
+
+Console.WriteLine("Input Text: I am really sad");
+Console.WriteLine("Mood: " + moodStatus);
+Console.ReadLine();
